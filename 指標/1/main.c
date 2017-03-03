f75abfb9a7a4d80601d8a13b66ea9f3df0d6b754
Dennis840822 <sj340821@gmail.com>
2017-03-03 15:24:10 +0800

    V0.1

---
+++ 指標/1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	int a,b=5;
+	double c=3.14;
+	printf("a=%4d, sizeof(a)=%d, 位址為%d\n",a,sizeof(a),&a);
+	printf("b=%4d, sizeof(b)=%d, 位址為%d\n",b,sizeof(b),&b);
+	printf("c=%4.2f, sizeof(c)=%d, 位址為%d\n",c,sizeof(c),&c);
+	system("PAUSE");
+	return 0;
+}
---
+++ 指標/3/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	/*int *ptr,num=20;
+	ptr = &num;
+	
+	printf("num=%d, &num=%p\n\n",num,&num);
+	printf("*ptr=%d, ptr=%p, &ptr=%p\n\n",*ptr,ptr,&ptr);
+	system("PAUSE");*/
+	
+	int a=5,b=3;
+	int *ptr;
+	
+	ptr=&a;
+	
+	printf("&a=%p, &ptr=%p, ptr=%p, *ptr=%d\n",&a,&ptr,ptr,*ptr);
+	ptr=&b;
+	printf("&b=%p, &ptr=%p, ptr=%p, *ptr=%d\n",&b,&ptr,ptr,*ptr);
+	return 0;
+}
