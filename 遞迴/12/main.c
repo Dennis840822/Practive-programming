03382d73f1e049b7ef492f2f52e23f89a2aececf
Dennis840822 <sj340821@gmail.com>
2017-03-07 16:15:49 +0800

    v0.1

---
+++ 遞迴/12/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define WORD "Think of all the things \
+we've shared and seen. \n"
+int main(int argc, char *argv[]) 
+{
+	printf(WORD);
+	
+	system("PAUSE");
+	return 0;
+}
