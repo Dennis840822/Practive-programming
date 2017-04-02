01f9f93c3248cad1a3857099b5a3292716c06311
Dennis840822 <sj340821@gmail.com>
2017-04-02 14:36:28 +0800

    v0.1

---
+++ 指標/5/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	int *ptri;
+	char *ptrc;
+	
+	printf("sizeof(ptri)=%d\n",sizeof(ptri));
+	printf("sizeof(ptrc)=%d\n",sizeof(ptrc));
+	printf("sizeof(*ptri)=%d\n",sizeof(*ptri));
+	printf("sizeof(*ptrc)=%d\n",sizeof(*ptrc));
+	
+	system("PAUSE");
+	return 0;
+}
