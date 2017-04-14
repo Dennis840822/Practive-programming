934c4af763be75b124672d0d580b6129dfe76eb9
Dennis840822 <sj340821@gmail.com>
2017-04-14 15:05:55 +0800

    v0.1

---
+++ 指標/13/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	int i,a[5]={32,16,35,65,52};
+	printf("a=%p\n",a);
+	printf("&a=%p\n",&a);
+	
+	for(i=0;i<5;i++)
+		printf("&a[%d]=%p\n",i,&a[i]);
+		
+	system("PAUSE");
+	return 0;
+}
