4342c544e7f353055bb1c8faf7064f0faf16e517
Dennis840822 <sj340821@gmail.com>
2017-04-14 15:30:58 +0800

    v0.1

---
+++ 指標/15/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	int a[3] = {5,7,9};
+	printf("a[0]=%d, *(a+0)=%d\n",a[0],*(a+0));
+	printf("a[1]=%d, *(a+1)=%d\n",a[1],*(a+1));
+	printf("a[2]=%d, *(a+2)=%d\n",a[2],*(a+2));
+	return 0;
+}
