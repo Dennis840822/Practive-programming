8b70ecd1dfaef889ae22f2cb774677e0f9910365
Dennis840822 <sj340821@gmail.com>
2017-04-15 13:57:58 +0800

    v0.1

--- 指標/16/main.c
+++ 指標/16/main.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 	int a[3] = {5,7,9};
 	int i,sum=0;
 	for(i=0;i<3;i++)
-		sum += *(a+i);
+		sum += *(a+i);//  =>  sum = sum + *(a+i); 
 	printf("sum=%d\n",sum);
 	
 	system("PAUSE");
