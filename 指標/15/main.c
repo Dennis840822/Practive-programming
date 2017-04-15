0656c4aef7038ff0e037558b83e194f6942c2ebc
Dennis840822 <sj340821@gmail.com>
2017-04-15 13:49:54 +0800

    v0.1

--- 指標/15/main.c
+++ 指標/15/main.c
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]) 
 {
 	int a[3] = {5,7,9};
-	printf("a[0]=%d, *(a+0)=%d\n",a[0],*(a+0));//*(a+0) = a[0] 
+	printf("a[0]=%d, *(a+0)=%d\n",a[0],*(a+0));//在程式裡a+i則代表a[i]的位址 
 	printf("a[1]=%d, *(a+1)=%d\n",a[1],*(a+1));
 	printf("a[2]=%d, *(a+2)=%d\n",a[2],*(a+2));
 	return 0;
