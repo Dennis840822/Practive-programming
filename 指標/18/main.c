0ebcd85e080614c9221f7b8951c4e57f2e91fe0f
Dennis840822 <sj340821@gmail.com>
2017-04-15 14:28:16 +0800

    v0.1

---
+++ 指標/18/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	int a[3] = {5,7,9};
+	int i,sum=0;
+	int *ptr = a;
+	for(i=0;i<3;i++)
+		sum += *(ptr++);
+	printf("sum=%d\n",sum);
+	
+	system("PAUSE");
+	return 0;
+}
