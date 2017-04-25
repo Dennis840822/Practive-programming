3e037ae45bcd2951706e5cce004d68301632b049
Dennis840822 <sj340821@gmail.com>
2017-04-25 15:46:44 +0800

    v0.1

--- 指標/22/main.c
+++ 指標/22/main.c
@@ -8,9 +8,11 @@ int main(int argc, char *argv[])
 	char name[20];//name是指標常數，值無法更改 
 	char *ptr = "How are you";
 	printf("what`s your name?");
-	gets(++name);//拿到鍵盤輸入的值並存放在字元name裡面 
+	gets(++name);//拿到鍵盤輸入的值並存放在字元name裡面
+	//name是指標常數無法做數值更改 
 	printf("Hi, %s, ",name);
-	puts(++ptr);//直接印出指標變數ptr的值 
+	puts(++ptr);//直接印出指標變數ptr的值
+	//ptr是指標變數能做數值更改 
 	
 	system("PAUSE");
 	return 0;
