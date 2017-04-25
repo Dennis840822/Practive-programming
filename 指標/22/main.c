401b446ef1f7939ca503a7caf10e0fa1cf6dafc6
Dennis840822 <sj340821@gmail.com>
2017-04-25 15:39:38 +0800

    v0.1

--- 指標/22/main.c
+++ 指標/22/main.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+//第20個程式碼作變更 
 int main(int argc, char *argv[]) 
 {
 	char name[20];//name是指標常數，值無法更改 
 	char *ptr = "How are you";
 	printf("what`s your name?");
-	gets(name);//拿到鍵盤輸入的值並存放在字元name裡面 
+	gets(++name);//拿到鍵盤輸入的值並存放在字元name裡面 
 	printf("Hi, %s, ",name);
-	puts(ptr);//直接印出指標變數ptr的值 
+	puts(++ptr);//直接印出指標變數ptr的值 
 	
 	system("PAUSE");
 	return 0;
