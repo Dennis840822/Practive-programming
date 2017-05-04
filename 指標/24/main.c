6fb216d5f44819181a2abb1605a3dc5f9545f5e4
Dennis840822 <sj340821@gmail.com>
2017-05-04 13:56:37 +0800

    practive

--- 指標/24/main.c
+++ 指標/24/main.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 	p = &n;
 	pp = &p;
 	printf("n=%d, &n=%p, *p=%d, p=%p, &p=%p\n",n,&n,*p,p,&p);
-	printf("**pp=%d, **p=%p, pp=%p, &pp=%p\n",**pp,*pp,pp,&pp);
+	printf("**pp=%d, *pp=%p, pp=%p, &pp=%p\n",**pp,*pp,pp,&pp);
 	
 	system("PAUSE");
 	return 0;
