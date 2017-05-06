86352e3578ef080266e96ba8f1e98d2229c91355
Dennis840822 <sj340821@gmail.com>
2017-05-06 14:34:01 +0800

    practive

--- 結構/1/main.c
+++ 結構/1/main.c
@@ -3,6 +3,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
+	struct data{
+		char name[10];
+		int math;
+	}student;
+	
+	printf("請輸入學生姓名：");
+	gets(student.name);
+	printf("請輸入成績：");
+	scanf("%d",&student.math);
+	
+	printf("姓名：%s\n",student.name);
+	printf("姓名：%d\n",student.math);
+	
+	system("PAUSE");
 	return 0;
 }
