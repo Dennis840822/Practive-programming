990f9771749ebba20a8f56f1d7d5d2afb7b52cd2
Dennis840822 <sj340821@gmail.com>
2017-09-11 11:27:30 +0800

    practive

--- 指標/12/main.c
+++ 指標/12/main.c
@@ -5,6 +5,13 @@
 
 int main(int argc, char *argv[]) 
 {
+	int i,a[5] = {32,16,35,65,52};
+	printf("a=%p\n",a);
+	printf("&a=%p\n",&a);
 	
+	for(i=0;i<5;i++)
+		printf("&a[%d]=%p\n",i,&a[i]);
+	
+	system("PAUSE");
 	return 0;
 }
